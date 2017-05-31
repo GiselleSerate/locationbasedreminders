b1389f9c19096228a7b7a23fc9b03ec432e44264
cssummer17 <cssummer17@c93.clinic.cs.hmc.edu>
2017-05-31 15:36:21 -0700

    Updated manifest with internet permissions.

--- MapsAPI/app/src/main/java/com/example/cssummer17/mapsapi/MainActivity.java
+++ MapsAPI/app/src/main/java/com/example/cssummer17/mapsapi/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.cssummer17.mapsapi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+TextView mTxtDisplay;
+ImageView mImageView;
+mTxtDisplay = (TextView) findViewById(R.id.txtDisplay);
+String url = "http://my-json-feed";
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,3 +18,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
+
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+        (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+@Override
+public void onResponse(JSONObject response) {
+        mTxtDisplay.setText("Response: " + response.toString());
+        }
+        }, new Response.ErrorListener() {
+
+@Override
+public void onErrorResponse(VolleyError error) {
+        // TODO Auto-generated method stub
+
+        }
+        });
+
+// Access the RequestQueue through your singleton class.
+        MySingleton.getInstance(this).addToRequestQueue(jsObjRequest);
