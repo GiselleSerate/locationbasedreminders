58a795137487a4e44a2c5921a81e47bf7754d194
GiselleSerate <giselle.serate@gmail.com>
2017-06-02 15:01:37 -0700

    I have added comments to PlacePicker. Possibly I should leave it alone and start another project to get the other things to work.

--- PlacePicker/app/src/main/java/com/example/cssummer17/placepicker/MainActivity.java
+++ PlacePicker/app/src/main/java/com/example/cssummer17/placepicker/MainActivity.java
@@ -13,17 +13,19 @@ import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlacePicker;
+// the following two set lat and long. if you by default want your current location, don't bother.
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 
 
 public class MainActivity extends AppCompatActivity {
-    private static final int PLACE_PICKER_REQUEST = 1;
-    private TextView mName;
-    private TextView mAddress;
+    private static final int PLACE_PICKER_REQUEST = 1; // internally recognized code for the request we're doing
+    private TextView mName;         // we write our place name to this var
+    private TextView mAddress;      // we write our place address to this var
     private TextView mAttributions;
-    private static final LatLngBounds BOUNDS_MOUNTAIN_VIEW = new LatLngBounds(
-            new LatLng(37.398160, -122.180831), new LatLng(37.430610, -121.972090));
+    // the following two set lat and long. if you by default want your current location, don't bother.
+//    private static final LatLngBounds BOUNDS_MOUNTAIN_VIEW = new LatLngBounds(
+//            new LatLng(37.398160, -122.180831), new LatLng(37.430610, -121.972090));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,16 +37,17 @@ public class MainActivity extends AppCompatActivity {
         Button pickerButton = (Button) findViewById(R.id.pickerButton);
         pickerButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                try {
+            public void onClick(View v) { // does this thing when you click on it (overriding whatever click behavior normally does)
+                try { // request an intent
                     PlacePicker.IntentBuilder intentBuilder =
                             new PlacePicker.IntentBuilder();
-                    intentBuilder.setLatLngBounds(BOUNDS_MOUNTAIN_VIEW);
+                    // sets lat and long. if you by default want your current location, don't bother.
+//                    intentBuilder.setLatLngBounds(BOUNDS_MOUNTAIN_VIEW);
                     Intent intent = intentBuilder.build(MainActivity.this);
-                    startActivityForResult(intent, PLACE_PICKER_REQUEST);
+                    startActivityForResult(intent, PLACE_PICKER_REQUEST); // start the intent and ask for a place
 
                 } catch (GooglePlayServicesRepairableException
-                        | GooglePlayServicesNotAvailableException e) {
+                        | GooglePlayServicesNotAvailableException e) {  // they were not happy with our intentions :(
                     e.printStackTrace();
                 }
             }
@@ -53,11 +56,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode,
-                                    int resultCode, Intent data) {
+                                    int resultCode, Intent data) { // do this when you get back what somebody gave you back
 
         if (requestCode == PLACE_PICKER_REQUEST
-                && resultCode == Activity.RESULT_OK) {
+                && resultCode == Activity.RESULT_OK) { // is it what the place picker gave me back and is it not broken?
 
+            // define your variables to write in
             final Place place = PlacePicker.getPlace(this, data);
             final CharSequence name = place.getName();
             final CharSequence address = place.getAddress();
