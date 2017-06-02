83d83162e3fca1579ff953444649476f0ac3bd07
GiselleSerate <giselle.serate@gmail.com>
2017-06-02 16:30:51 -0700

    Currently doing all that crashy goodness. I'll have to see what's going wrong.

---
+++ AutofillPlaceTypes/app/src/main/java/com/example/cssummer17/autofillplacetypes/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.cssummer17.autofillplacetypes;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.text.Html;
+
+import com.example.cssummer17.autofillplacetypes.utilities.NetworkUtils;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.GeoDataApi;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int PLACE_PICKER_REQUEST = 1; // internally recognized code for the request we're doing
+    private TextView mName;                         // we write our place name to this var
+    private TextView mUrlDisplayTextView;
+    private TextView mSearchResultsTextView;      // we write our place address to this var
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    /**
+     * This method retrieves the search text from the EditText, constructs the
+     * URL (using {@link NetworkUtils}) for the github repository you'd like to find, displays
+     * that URL in a TextView, and finally fires off an AsyncTask to perform the GET request using
+     * our {@link GithubQueryTask}
+     */
+    private void makeGithubSearchQuery() {
+        URL githubSearchUrl = NetworkUtils.buildUrl();
+        mUrlDisplayTextView.setText(githubSearchUrl.toString());
+        // COMPLETED (4) Create a new GithubQueryTask and call its execute method, passing in the url to query
+        new GithubQueryTask().execute(githubSearchUrl);
+    }
+
+    // COMPLETED (1) Create a class called GithubQueryTask that extends AsyncTask<URL, Void, String>
+    public class GithubQueryTask extends AsyncTask<URL, Void, String> {
+
+        // COMPLETED (2) Override the doInBackground method to perform the query. Return the results. (Hint: You've already written the code to perform the query)
+        @Override
+        protected String doInBackground(URL... params) {
+            URL searchUrl = params[0];
+            String githubSearchResults = null;
+            try {
+                githubSearchResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return githubSearchResults;
+        }
+
+        // COMPLETED (3) Override onPostExecute to display the results in the TextView
+        @Override
+        protected void onPostExecute(String githubSearchResults) {
+            if (githubSearchResults != null && !githubSearchResults.equals("")) {
+                mSearchResultsTextView.setText(githubSearchResults);
+            }
+        }
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemThatWasClickedId = item.getItemId();
+        if (itemThatWasClickedId == R.id.action_search) {
+            makeGithubSearchQuery();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
---
+++ AutofillPlaceTypes/app/src/main/java/com/example/cssummer17/autofillplacetypes/utilities/NetworkUtils.java
@@ -0,0 +1,88 @@
+package com.example.cssummer17.autofillplacetypes.utilities;
+
+import android.net.Uri;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+/**
+ * Created by cssummer17 on 6/2/17.
+ */
+
+public class NetworkUtils {
+
+    final static String GITHUB_BASE_URL =
+            "https://maps.googleapis.com/maps/api/place/nearbysearch";
+
+    final static String PARAM_FORMAT = "json";
+
+    /*
+     * The sort field. One of stars, forks, or updated.
+     * Default: results are sorted by best match if no field is specified.
+     */
+    final static String PARAM_LOCATION = "location";
+    final static String location = "-33.8670522,151.1957362"; // lat/long of the thing.
+
+    final static String PARAM_RADIUS = "radius";
+    final static String radius = "500";
+
+
+    final static String PARAM_TYPE = "type";
+    final static String type = "restaurant";
+
+
+//    of form https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=YOUR_API_KEY
+
+    /**
+     * Builds the URL used to query GitHub.
+     * @return The URL to use to query the GitHub.
+     */
+    public static URL buildUrl() {
+        Uri builtUri = Uri.parse(GITHUB_BASE_URL).buildUpon()
+                .appendPath(PARAM_FORMAT)
+                .appendQueryParameter(PARAM_LOCATION, location)
+                .appendQueryParameter(PARAM_RADIUS, radius)
+                .appendQueryParameter(PARAM_TYPE, type)
+                .build();
+
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return url;
+    }
+
+    /**
+     * This method returns the entire result from the HTTP response.
+     *
+     * @param url The URL to fetch the HTTP response from.
+     * @return The contents of the HTTP response.
+     * @throws IOException Related to network and stream reading
+     */
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+
+}
