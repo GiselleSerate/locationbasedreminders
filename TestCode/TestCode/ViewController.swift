864fa1f31fc3c43a01e7a13c6283262adc71f878
GiselleSerate <giselle.serate@gmail.com>
2017-06-07 10:36:45 -0700

    Went through some Udacity tutorialness. Also added some CocoaPods to the project, and a Google Calendar client_id json.

--- TestCode/TestCode/ViewController.swift
+++ TestCode/TestCode/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    
+    @IBOutlet weak var recordButton: UIButton!
+    
+    @IBOutlet weak var stopRecordButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        stopRecordButton.isEnabled = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(_ sender: Any) {
+        print("The record button was pressed.")
+        recordingLabel.text = "recording in progress"
+        stopRecordButton.isEnabled = true
+        recordButton.isEnabled = false
+    }
+    
+    @IBAction func stopRecord(_ sender: Any) {
+        print("You just stopped recording.")
+        recordButton.isEnabled = true
+        stopRecordButton.isEnabled = false
+    }
 
 }
 
