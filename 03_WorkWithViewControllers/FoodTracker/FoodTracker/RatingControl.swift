553c41d25818f7f899d93466736ae0a4e57815cd
GiselleSerate <giselle.serate@gmail.com>
2017-06-09 16:35:15 -0700

    Halfway through star implementation. They are not stars yet.

---
+++ 03_WorkWithViewControllers/FoodTracker/FoodTracker/RatingControl.swift
@@ -0,0 +1,65 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by cssummer17 on 6/9/17.
+//  Copyright © 2017 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class RatingControl: UIStackView {
+    
+    //MARK: Properties
+    private var ratingButtons = [UIButton]()
+    var rating = 0
+    
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0)
+    @IBInspectable var starCount: Int = 5
+
+    
+    //MARK: Initialization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtons()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupButtons()
+    }
+    
+    
+    //MARK: Private Methods
+    
+    private func setupButtons() {
+        for _ in 0..<starCount {
+            // Create a button.
+            let button = UIButton()
+            button.backgroundColor = UIColor.red
+            
+            // Add constraints.
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            
+            // Setup the button action
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+            
+            // Put the button in the stack.
+            addArrangedSubview(button)
+            
+            // Put the button in the rating button array. 
+            ratingButtons.append(button)
+        }
+    }
+    
+    
+    //MARK: Button Action
+    
+    func ratingButtonTapped(button: UIButton) {
+        print("we push the button yay")
+    }
+
+}
